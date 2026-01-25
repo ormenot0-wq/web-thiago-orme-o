b20ba92804224a6859771a19c15c64d6656e0139
thiago ormeño <ormenot0@gmail.com>
2026-01-25 20:20:27 -0300

    inicio clase 2

---
+++ styles/documento.js
@@ -0,0 +1,32 @@
+let nombre ="thiago"
+
+    console.log("declaro variable: ",nombre);
+ 
+    console.log("reasigno valor");
+    nombre = "pedro";
+
+    console.log(nombre);
+
+    var noMeUses = "hola";
+
+    const dni = 4324354121;
+    console.log(dni, typeof dni)
+
+    let comillaD = "HOLA"
+    let comillaS = "diana"
+    let backtick = "miercoles"
+
+    let oracion = "pepe dije"
+    let prueba = "fiona dijo" + "llanura humeda"
+
+    console.log(oracion)
+
+   let saldo = párseInt(prompt("ingrese su saldo en numero"))
+    if (saldo < 0){
+        alert("tenes saldo negativo")
+    } else {
+        alert("usted es menor de edad")
+    }
+
+
+
---
+++ styles/fluido.js
@@ -0,0 +1,13 @@
+const pedido = prompt("¿que queres tomar?")
+
+if (pedido==="toca"){
+  alert("Aca tenes fofa")
+} else if(pedido==="jugo"){
+    alert("toma jugo")
+} else{
+    alert("arreglate con agua")
+}
+
+const lista = [1,2,3,4 ] 
+ 
+console.log(lista)
